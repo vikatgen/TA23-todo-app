46ac0110dfcb48ec08e1d7ae3a72747cd73d0c3e
Gen Vikat <gen@veebisepad.ee>
2024-10-04 17:04:14 +0300

    inital setup

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3006
+const animals = ['Koer', 'Inimene', 'Jõehobu', 'Lammas']
+
+const app = express()
+
+app.get('/animals', (request, response) => {
+    response.status(200).json(animals)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${ PORT }`)
+})
